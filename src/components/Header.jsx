3f76618273abd897d6594aad519ea0b675323655
VitaFrancesco <vitafrancesco00@gmail.com>
2025-01-09 17:16:20 +0100

    ADD: axios call

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
+import { NavLink } from "react-router-dom"
+
 export default function Header() {
     return (
         <header>
             <nav>
-                HEADER
+                <div className="logo">
+                    <div className="logoSfera">F</div>
+                    <div className="logoSfera">I</div>
+                    <div className="logoSfera">L</div>
+                    <div className="logoSfera">M</div>
+                </div>
             </nav>
         </header>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function Home() {
+    const [movies, setMovies] = useState([])
+    const moviesUrl = "http://localhost:3000/movies"
+    useEffect(() => {
+        axios.get(moviesUrl)
+            .then((res) => {
+                setMovies(res.data)
+            }
+            ).catch((err) => console.error(err))
+    }, [])
+
     return (
         <>
             <h1>Homepage</h1>
+            <div>{
+                movies.map((movie, i) => {
+                    return (
+                        <h3 key={i}>{movie.title}</h3>
+                    )
+                })
+            }</div>
         </>
     )
 }
